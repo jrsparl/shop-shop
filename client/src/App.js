9dace0415dc2d4f6272f4f01e813be457aabd72e
Jrsparl <john@sparlingmusic.com>
2021-02-07 13:16:14 -0500

    refactored for redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
+import store from "./utils/store"
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
